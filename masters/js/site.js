034d07802e0275587fb39ff89783dab600beddde
Luis Teixeira <luispteixeira@gmail.com>
2013-11-11 16:56:59 +0000

    Bootstrakers Theme for Wordpress - A clean responsive theme for wordpress based on Strakers with Bootstrap 3

---
+++ masters/js/site.js
@@ -0,0 +1,9 @@
+
+//@codekit-prepend "../bootstrap/js/transition.js";
+//@codekit-prepend "../bootstrap/js/collapse.js";
+
+jQuery(document).ready(function($) {
+	
+	// Your JavaScript goes here
+});
+
